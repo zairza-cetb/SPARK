5c734ea8293cd37666d7f5f68b378e90400b1448
aditi018 <dasaditi018@gmail.com>
2022-10-04 01:22:11 +0530

    updated signup ui and added hover effects

--- src/pages/auth/SignUpPage.js
+++ src/pages/auth/SignUpPage.js
@@ -247,10 +247,10 @@ export default function SignUpPage(props) {
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
-                      viewBox="0 0 24 24"
+                      viewBox="0 0 30 18"
                       strokeWidth={1.5}
                       stroke="currentColor"
-                      className="w-6 h-6 text-primary"
+                      className="w-6 h-6 text-primary group-hover:stroke-white"
                     >
                       <path
                         strokeLinecap="round"
@@ -265,11 +265,15 @@ export default function SignUpPage(props) {
                     id="number"
                     value={phoneNumber}
                     onChange={(e) => handleNumberChange(e)}
-                    class="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none w-full pl-10 transition focus:ring-4 focus:ring-secondary"
+                    class="rounded w-full text-gray-800 focus:outline-none focus:border-primary transition duration-500 px-3 pb-3 tracking-widest border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                    focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+                    disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+                    invalid:border-pink-500 invalid:text-pink-600
+                    focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
                   />
 
                   <button
-                    className="w-full mt-3 bg-white font-bold py-2 rounded hover:shadow-xl transition duration-200 text-primary border-0"
+                    className="bg-white font-bold py-2 rounded hover:shadow-xl transition duration-200 text-primary hover:bg-light"
                     onClick={sendOTP}
                   >
                     Next
